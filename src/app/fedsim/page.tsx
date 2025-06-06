ffe504d5e45a16b53e45c4c1e49a84f06d7d171d
xentoshi <xentoshi@protonmail.com>
2025-06-06 14:32:00 +0900

    Fix: remove props from FedSimFeature usage

--- src/app/fedsim/page.tsx
+++ src/app/fedsim/page.tsx
@@ -1,15 +1,12 @@
 // src/app/fedsim/page.tsx
 'use client'
 
-import { useGameEngine } from '@/components/fedsim/fedsim-engine'
 import { FedSimFeature } from '@/components/fedsim/fedsim-feature'
 
 export default function Page() {
-  const { state, simulateTurn } = useGameEngine()
-
   return (
     <div className="flex flex-col gap-4 p-8">
-      <FedSimFeature state={state} simulateTurn={simulateTurn} />
+      <FedSimFeature />
     </div>
   )
 }
